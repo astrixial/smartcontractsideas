f68dd0ef18424f5f5c649e13810b42d084ba6f0d
astrixial <bitcoinplus2@gmail.com>
2016-07-21 15:54:29 -0500

    Create faucetifinite .sol

---
+++ faucetifinite .sol
@@ -0,0 +1,37 @@
+// Save this in Faucet.sol
+//the original faucet code is this 
+// the question is possible creat a faucet infinite ??
+/// futher code
+contract Faucet {
+    address owner;
+    uint256 sendAmount;
+
+    function Faucet() {
+        owner = msg.sender;
+        sendAmount = 1000000000000000000;
+    }
+
+    function getBalance() returns (uint) {
+         return address(this).balance;
+    }
+
+    function getWei() returns (bool) {
+        msg.sender.send(sendAmount);
+        return true;
+    }
+
+    function sendWei(address toWhom) returns (bool) {
+        toWhom.send(sendAmount);
+        return true;
+    }
+
+    function getSendAmount() returns (uint256) {
+        return sendAmount;
+    }
+
+    function killMe() {
+        if (msg.sender == owner) {
+            suicide(owner);
+        }
+    }
+}
