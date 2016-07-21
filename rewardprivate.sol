6b4bdc1d8e72a4c038803a83b511cfb02d779926
astrixial <bitcoinplus2@gmail.com>
2016-07-21 15:50:36 -0500

    Update rewardprivate.sol

--- rewardprivate.sol
+++ rewardprivate.sol
@@ -1,31 +1,6 @@
-/*
-This file is part of the DAO.
-
-The DAO is free software: you can redistribute it and/or modify
-it under the terms of the GNU lesser General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-The DAO is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU lesser General Public License for more details.
-
-You should have received a copy of the GNU lesser General Public License
-along with the DAO.  If not, see <http://www.gnu.org/licenses/>.
-*/
 
 
-/*
-  Sample Proposal from a Contractor to the DAO including a reward towards the
-  DAO.
-
-  Feel free to use as a template for your own proposal.
-*/
-
-import "./Offer.sol";
-
-contract RewardOffer is Offer {
+contract RewardOfferprivate  is Offer {
 
     uint rewardDivisor;
     uint deploymentReward;
